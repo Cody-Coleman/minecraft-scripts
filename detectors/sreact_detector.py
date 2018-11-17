3e87bab2a10fabe29606dd7f8bba8d286b88f444
Cody Coleman <codyc@qualtrics.com>
2018-11-16 19:47:52 -0700

    more fix

--- detectors/sreact_detector.py
+++ detectors/sreact_detector.py
@@ -5,12 +5,15 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-player_dict = {'waffle_man': 17, 'zzz': 27, 'jojo_pie': 22}
+player_dict = {'waffle_man': 17, 'zzz': 27, 'jojo_pie': 22, 'ziblez'}
 action_dict = {'joined': GPIO.HIGH, 'left': GPIO.LOW}
 
 if __name__ == '__main__':
-    player_name = sys.argv[1]
-    pin = player_dict[player_name]
+    player_name = sys.argv[1].lower()
+    if player_name in player_dict:
+        pin = player_dict[player_name]
+    else:
+        raise SystemExit("Player not found")
     action = action_dict[sys.argv[2]]
     print("Player: {}\tPin: {}\tAction: {}".format(player_name, pin, action))
     GPIO.setup(pin, GPIO.OUT)
