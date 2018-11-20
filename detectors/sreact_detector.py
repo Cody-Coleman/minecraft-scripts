d0d2c970d03ceb7e8433e450308b674eb63c86a0
Cody Coleman <codyc@qualtrics.com>
2018-11-20 15:58:09 -0700

    adding unknown

--- detectors/sreact_detector.py
+++ detectors/sreact_detector.py
@@ -6,7 +6,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-player_dict = {'waffleman1793': 13, 'zzz': 6, 'jojo_pie': 5, 'ziblez': 22}
+player_dict = {'waffleman1793': 13, 'zzz': 6, 'jojo_pie': 5}
 action_dict = {'joined': GPIO.HIGH, 'left': GPIO.LOW}
 
 if __name__ == '__main__':
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         if player_name in player_dict:
             pin = player_dict[player_name]
         else:
-            raise SystemExit("Player not found")
+            pin = 22
         action = action_dict[sys.argv[2]]
         print("Player: {}\tPin: {}\tAction: {}".format(player_name, pin, action))
         GPIO.setup(pin, GPIO.OUT)
